6de04f9706bf1f08494ab0e3402c987388593c7f
Deb <bhatrai.deb@gmail.com>
2020-09-28 18:56:27 +0530

    Updated Admin to supports custom user model.

---
+++ app/core/tests/test_amin.py
@@ -0,0 +1,40 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class TestAdminSite(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            name='Deb Prasad',
+            email='deb@gmail.com',
+            password='deb123'
+        )
+
+    def test_users_listed(self):
+        """Tests that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        result = self.client.get(url)
+
+        self.assertContains(result, self.user.name)
+        self.assertContains(result, self.user.email)
+
+    def test_user_change_page(self):
+        """Tests that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        result = self.client.get(url)
+
+        self.assertEquals(result.status_code, 200)
+
+    def test_create_user_page(self):
+        """Tests that the create user page works"""
+        url = reverse('admin:core_user_add')
+        result = self.client.get(url)
+
+        self.assertEquals(result.status_code, 200)
